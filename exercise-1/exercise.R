59b9469395e9ae0f27dec912832819e80e7b53ab
Carmelito Gutierrez <carmelitogutierrez@gmail.com>
2017-01-19 15:20:13 -0800

    Work on modules

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -2,27 +2,29 @@
 
 # Create a vector of the number of points the Seahawks scored in each game this
 # season (google "Seahawks" for the info!)
-
+points <- c(20, 20, 31, 31, 26, 5, 40, 10, 24, 31, 25, 26)
 
 # Create a vector of the number of points the Seahwaks have allowed to be scored
 # against them in each game this season
-
+enemy.points <- c(36, 25, 25, 24, 15, 14, 7, 38, 3, 34, 23, 6)
 
 # Combine your two vectors into a dataframe
-
+seahawk.data <- data.frame(points, enemy.points)
 
 # Create a new column "diff" that is the difference in points.
 # Hint: recall the syntax for assigning new elements (which in this case will be
 # a vector) to a list!
-
+seahawk.data$point.difference <- points - enemy.points
 
 # Create a new column "won" which is TRUE if the Seahawks won
-
+seahawk.data$won <- seahawk.data$point.difference > 0
 
 # Create a vector of the opponent names corresponding to the games played
-
+opponent.names <- c("Falcons", "Saints", "Bills", "Patriots", "Eagles", "Buccaneers", 
+                    "Panthers", "Packers", "Rams", "Cardinals", "49ers", "Lions")
 
 # Assign your dataframe rownames of their opponents
-
+seahawk.data$enemies <- opponent.names
 
 # View your data frame to see how it has changed!
+View(seahawk.data)
--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -1,25 +1,26 @@
 # Create a vector of 100 employees ("Employee 1", "Employee 2", ... "Employee 100)
 # Hint: use the `paste()` function to produce the list
-
+employees <- c("Employee ")
+employees <- paste(employees, 1:100)
 
 # Create a vector of 100 random salaries for the year 2014
 # Use the `runif()` function to pick a random number between 40000 and 50000
-
+prev.salaries <- c(runif(100, 40000, 50000))
 
 # Create a vector of 100 salaries in 2015 that have increased from 2014 by some amount
 # Hint: use `runif()` to add a random number to 2014's salaries. Starting from a
 # _negative_ number so that salaries may decrease!
-
+next.salaries <- c(prev.salaries + runif(100, -1000, 1000))
 
 # Create a data.frame 'salaries' by combining the 3 vectors you just made
 # Remember to set `stringsAsFactors=FALSE`!
-
+salaries <- data.frame(employees, prev.salaries, next.salaries, stringsAsFactors = FALSE)
 
 # Create a column 'raise' that stores the size of the raise between 2014 and 2015
-
+salaries$raise <- next.salaries - prev.salaries
 
 # Create a column 'got.raise' that is TRUE if the person got a raise
-
+salaries$got.raise <- salaries$raise > 0
 
 ### Retrieve values from your data frame to answer the following questions
 ### Note that you should get the value as specific as possible (e.g., a single
@@ -27,20 +28,22 @@
 
 # What was the 2015 salary of employee 57
 
+salaries$next.salaries[57]
 
 # How many employees got a raise?
-
+sum(salaries$got.raise)
 
 # What was the value of the highest raise?
+max(salaries$raise)
 
-
-# What was the "name" of the employee who received the highest raise?
-
+# What was the name of the employee who received the highest raise?
+salaries[salaries$raise == max(salaries$raise), "employees"]
 
 # What was the largest decrease in salaries between the two years?
-
+min(salaries$raise)
 
 # What was the name of the employee who recieved largest decrease in salary?
+salaries[salaries$raise == min(salaries$raise), "employees"]
 
 # What was the average salary increase?
 
